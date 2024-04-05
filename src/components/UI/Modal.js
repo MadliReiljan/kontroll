e43e4e2642755db4efac232cd8c8f01930bc5d2e
MadliReiljan <madlireiljan@gmail.com>
2024-04-05 12:55:18 +0300

    Ül 10

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
+import { CartContext } from '../../store/CartContext';
 
-const Modal = ({ isOpen, toggleModal, children }) => { 
+const Modal = ({ isOpen, toggleModal}) => { 
     const modalRef = useRef(null);
+    const { cartItems } = useContext (CartContext);
 
     useEffect(() => {
         if (isOpen) {
@@ -11,13 +13,22 @@ const Modal = ({ isOpen, toggleModal, children }) => {
         }
     }, [isOpen]);
 
+    const totalPrice = cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
+    const formattedPrice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR'}).format(totalPrice);
+
     return ( 
         <dialog ref={modalRef} className="modal">
             <h2>Your cart</h2>
             <ul className="cart-items">
-                {children}
+                {cartItems.map(item => ( 
+                    <li key={item.id}>
+                        <span>{item.name}</span>
+                        <span> - </span>
+                        <span>{item.quantity}</span>
+                    </li>
+                ))}
             </ul>
-            <p className="cart-total"></p>
+            <p className="cart-total">{formattedPrice}</p>
             <p className="modal-actions">
                 {}
                 <button className="text-button" onClick={toggleModal}>Close</button>
